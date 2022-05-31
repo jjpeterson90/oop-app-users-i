71513e0109c653ef5c41b099e999becbc64ffd8a
Justin <jjpeterson90@hotmail.com>
2022-05-31 11:17:31 -0500

    initial completion

--- User.py
+++ User.py
@@ -1,1 +1,15 @@
-# your User class goes here
+class User:
+    def __init__(self, name, email_address, drivers_license_number):
+        self.name = name
+        self.email = email_address
+        self.license = drivers_license_number
+        
+    def __str__(self):
+        return f"""\
+            name: {self.name}
+            email: {self.email}
+            license: {self.license}\
+            """
+            
+#justin = User('Justin', 'email@address.com', '1112223333')
+#print(justin)
